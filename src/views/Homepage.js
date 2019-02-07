b23c88132112cc621644975ac511e30dcd9ab98d
hassifmohd <mohdhassif.jabir@tyrellsys.com.my>
2019-02-08 00:05:23 +0800

    hello world

---
+++ src/views/Homepage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <h1>Hello world</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Homepage;
