934706ef1f872125d7183cba61b8c3817a67b8bb
hassifmohd <mohdhassif.jabir@tyrellsys.com.my>
2019-06-29 03:55:30 +0800

    do prettier + time format

--- src/views/AboutUs.js
+++ src/views/AboutUs.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
 
 class AboutUs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <Container>
-                    <Row>
-                        <Col xs="1"></Col>
-                        <Col xs="10">
-                            <p>Assalamualaikum.</p>
-                            <p>Ini adalah aplikasi waktu solat Malaysia.</p>
-                            <p>Data adalah daripada JAKIM.</p>
-                        </Col>
-                        <Col xs="1"></Col>
-                    </Row>
-                </Container>
-            </React.Fragment>
-        )
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Container>
+          <Row>
+            <Col xs="12">
+              <p>Assalamualaikum.</p>
+              <p>Ini adalah aplikasi waktu solat Malaysia.</p>
+              <p>Data adalah daripada JAKIM.</p>
+            </Col>
+          </Row>
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
 
 export default AboutUs;
